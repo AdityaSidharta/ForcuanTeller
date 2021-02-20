4bc33ce26a4d0339f585be43360c6e9531428811
Aditya Sidharta <aditya.sidharta@go-jek.com>
2021-02-20 17:09:35 +0800

    [Adi] Update scheduler

--- forcuanteller/main/main.py
+++ forcuanteller/main/main.py
@@ -2,16 +2,11 @@ from forcuanteller.main import loader, transformer, reporter, sender
 from forcuanteller.main.utils.gmail import validate_gmail
 from forcuanteller.main.utils.logger import logger
 from forcuanteller.main.utils.runner import runner
+import schedule
+import time
 
 
-def main():
-    sender_address = input("Input your sender gmail address: ")
-    sender_password = input("Input your sender gmail password: ")
-    receiver_address = input("Input your receiver gmail address: ")
-
-    validate_gmail(sender_address)
-    validate_gmail(receiver_address)
-
+def task(sender_address, sender_password, receiver_address):
     run_id = runner.run_id
     logger.info("Run ID : {}".format(run_id))
 
@@ -30,6 +25,22 @@ def main():
     logger.info("Running sender...")
     sender.main(run_id, sender_address, sender_password, receiver_address)
     logger.info("Finishing sender...")
+
+
+def main():
+    sender_address = input("Input your sender gmail address: ")
+    sender_password = input("Input your sender gmail password: ")
+    receiver_address = input("Input your receiver gmail address: ")
+
+    validate_gmail(sender_address)
+    validate_gmail(receiver_address)
+
+    schedule.every(60).seconds.do(
+        task, sender_address=sender_address, sender_password=sender_password, receiver_address=receiver_address
+    )
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
 
 
 if __name__ == "__main__":
--- forcuanteller/main/utils/gmail.py
+++ forcuanteller/main/utils/gmail.py
@@ -1,4 +1,10 @@
 def validate_gmail(gmail_address):
-    assert isinstance(gmail_address, str)
-    assert gmail_address.endswith("@gmail.com")
-    assert len(gmail_address) > 10
+    assert isinstance(gmail_address, str), "Gmail address should be a string, receives: {} instead".format(
+        gmail_address
+    )
+    assert gmail_address.endswith("@gmail.com"), "Gmail address should end in @gmail.com, receives: {} instead".format(
+        gmail_address
+    )
+    assert len(gmail_address) > 10, "Gmail address should have at least 11 characters, receives: {} instead".format(
+        gmail_address
+    )
